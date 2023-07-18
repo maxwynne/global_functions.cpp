2529a0df78e6215fb2b3f519516281ea8d626880
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-18 22:27:01 +0100

    Create global_variable_1.cpp

---
+++ global_variable_1.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int a = 5;
+
+int main()
+{
+
+    cout << "The value of a is"<< a <<"\n";
+
+    a--;
+
+    cout << "The value of a is "<< a <<"\n";
+
+    cout << "The value of a is "<< a <<"\n";
+}
+return 0;
